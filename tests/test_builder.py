89ab666628564c7b7870cbf197b471f63fcaf0ea
The <at.thealchemist@gmail.com>
2021-12-06 14:50:36 +0300

    Вынес пути в константы, сделал их относительными корня проекта и немного отрефакторил

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -9,24 +9,26 @@ from modules.builder import KnobStripBuilder, StripBuilder
 from modules.entities.strip import StripDirection
 from modules.entities.knob import Knob, KnobRotation
 
-PROJECT_ROOT = ROOT = str(Path(__file__).parent.parent.absolute())
-
+from constants import DATA_DIR
+test_knob_file_path = f"{DATA_DIR}/knob.png"
+test_led_file_paths = [
+    f"{DATA_DIR}/led_off.png",
+    f"{DATA_DIR}/led_on.png",
+]
+test_result_file_path = f"{DATA_DIR}/test_led_horizontal.png"
 
 @pytest.fixture
 def led_images():
     return [
-        Image.open(p) for p in (f"{ROOT}/data/led_off.png", f"{ROOT}/data/led_on.png")
+        Image.open(p) for p in test_led_file_paths
     ]
 
 @pytest.fixture
 def test_knob():
-    return Knob(
-        image=Image.open(str(Path(f"{ROOT}/data/knob.png").absolute())),
-    )
+    return Knob(image=Image.open(test_knob_file_path))
 
 
 def test_strip_builder(led_images):
-    test_result_file_path = f"{ROOT}/data/test_led_horizontal.png"
     sb = StripBuilder(direction=StripDirection.HORIZONTAL)
     res = sb.build(
         items=led_images
@@ -34,18 +36,13 @@ def test_strip_builder(led_images):
     sb.save(res.image, file_path=test_result_file_path)
     assert attr.asdict(res.metadata) == {
         "direction": StripDirection.HORIZONTAL,
-        "file_paths": [
-            f"{ROOT}/data/led_off.png",
-            f"{ROOT}/data/led_on.png",
-        ],
+        "file_paths": test_led_file_paths,
         "frames_count": 2,
         "extra": {}
     }
     assert Path(test_result_file_path).is_file()
 
 def test_knob_strip_builder(test_knob):
-    test_result_file_path = f"{ROOT}/data/test_led_horizontal.png"
-
     sb = KnobStripBuilder(
         knob=test_knob
     )
@@ -59,7 +56,7 @@ def test_knob_strip_builder(test_knob):
         },
         "direction": StripDirection.VERTICAL,
         "file_paths": [
-            f"{ROOT}/data/knob.png",
+            test_knob_file_path
         ],
         "frames_count": 27
     }
