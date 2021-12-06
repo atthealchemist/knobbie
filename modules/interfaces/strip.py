8b9ef546c1c3b2f5d28817f9f335911001fa5472
The <at.thealchemist@gmail.com>
2021-12-06 14:18:41 +0300

    Refactored builder.py

--- modules/interfaces/strip.py
+++ modules/interfaces/strip.py
@@ -37,23 +37,3 @@ class StripBuilderInterface(metaclass=ABCMeta):
 
         Returns: `StripBuilderResult`
         """
-
-# class KnobStripBuilderInterface(StripBuilderInterface):
-
-#     @abstractmethod
-#     def build(self) -> KnobStripBuilderResult:
-#         """
-#         Функция генерирует стрип для кноба (полоску png для анимации кноба)
-
-#         Returns: `KnobStripBuilderResult`
-#         """
-
-#     @abstractmethod
-#     def process(self) -> List[Image]:
-#         """
-#         Функция выполняет необходимые преобразования с кнобом 
-#         (в данном случае используется для покадрового поворота кноба)
-
-#         Returns:
-#             `List[PIL.Image]` - Список преобразованных фреймов
-#         """
